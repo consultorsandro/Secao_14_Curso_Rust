21330fb7a80f1ee115cdf35ea7d910a50c2ecb61
Sandro Reis <consultorsandro@hotmail.com>
2025-06-04 19:51:18 -0300

    Class 232 Privet to Public

--- src/inventory.rs
+++ src/inventory.rs
@@ -1,19 +1,18 @@
-
-const FLOOR_SPACE: i32 = 1000;
+pub const FLOOR_SPACE: i32 = 1000;
 pub const MANAGER: &str = "Alice";
 
 #[derive(Debug)]
-enum ProductCategory {
+pub enum ProductCategory {
     Ladder,
     Hammer,
 }
 #[derive(Debug)]
-struct Item {
-    name: String,
-    category: ProductCategory,
-    quantity: u32,
+pub struct Item {
+    pub name: String,
+    pub category: ProductCategory,
+    pub quantity: u32,
 }
 
-fn talk_to_manager() {
+pub fn talk_to_manager() {
     println!("{} Hey, how's your coffee?", MANAGER);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,20 @@ mod inventory;
 mod orders;
 
 fn main() {
-    println!("The manager is: {}", inventory::MANAGER);
-    println!("The manager is: {}", orders::MANAGER);
+   println!("Our managers are {} and {}. We have {} square feet of floor space.", 
+            inventory::MANAGER,
+            orders::MANAGER,
+            inventory::FLOOR_SPACE
+   );
+    inventory::talk_to_manager();
+
+    let favorite_category = inventory::ProductCategory::Hammer;
+    println!("My favorite category is: {:?}", favorite_category);
+
+    let tall_ladder = inventory::Item {
+        name: String::from("Ladder-o-matic 2000"),
+        category: favorite_category,
+        quantity: 10,
+    };  
+    println!("{:#?}", tall_ladder);
 }
