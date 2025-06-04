d2fe212754eac922d6a51709253790dae30d2b63
Sandro Reis <consultorsandro@hotmail.com>
2025-06-04 20:04:37 -0300

    Class 233 Submodules

---
+++ src/inventory/products.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+pub enum ProductCategory {
+    Ladder,
+    Hammer,
+}
+#[derive(Debug)]
+pub struct Item {
+    pub name: String,
+    pub category: ProductCategory,
+    pub quantity: u32,
+}
--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ fn main() {
    );
     inventory::talk_to_manager();
 
-    let favorite_category = inventory::ProductCategory::Hammer;
+    let favorite_category = inventory::products::ProductCategory::Hammer;
     println!("My favorite category is: {:?}", favorite_category);
 
-    let tall_ladder = inventory::Item {
+    let tall_ladder = inventory::products::Item {
         name: String::from("Ladder-o-matic 2000"),
         category: favorite_category,
         quantity: 10,
