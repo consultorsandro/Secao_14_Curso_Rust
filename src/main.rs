17dedf39935e6f4f949750a01120caeff29620e4
Sandro Reis <consultorsandro@hotmail.com>
2025-06-05 09:19:45 -0300

    Atualiza a estrutura do código para melhor organização e legibilidade

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,13 @@ fn main() {
         "Our managers are {} and {}. We have {} square feet of floor space.",
         INVENTORY_MANAGER, ORDERS_MANAGER, FLOOR_SPACE
     );
-    talk_to_manager();
 
     let favorite_category = ProductCategory::Hammer;
     println!("My favorite category is: {:?}", favorite_category);
 
-    let tall_ladder = Item::new(String::from("Ladder-o-matic 2000"), favorite_category, 10);
+    let tall_ladder = Item::new(String::from("Ladder-o-matic 2000"),
+        favorite_category,
+        10,
+    );
     println!("{:#?}", tall_ladder);
 }
